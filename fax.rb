5f508c9cd73bff7d342c22e12654f7e2829c9e89
Drazen Loncaric <drazen@urbanvine.com>
2018-12-14 21:21:38 -0800

    fix objkey structure

--- fax.rb
+++ fax.rb
@@ -20,7 +20,7 @@ def get(event:, context:)
         pdf = SrFax.get_fax(fax[:FileName], 'IN')
         if pdf[:Status].upcase == 'SUCCESS'
           # Save the Retrieved Fax (base64 encoded) PDF document to our incoming documents S3 bucket path.
-          obj_key = "fax/#{fax[:EpochTime]}/#{fax[:FileName].split('|').first}-from-#{fax[:RemoteID]}-#{fax[:CallerID]}-pages-#{fax[:Pages]}.pdf"
+          obj_key = "fax/received/#{fax[:Filename][0..7]}/#{fax[:FileName].split('|').first}-from-#{fax[:RemoteID]}-#{fax[:CallerID]}-pages-#{fax[:Pages]}.pdf"
           
           obj = s3.bucket(ENV['INCOMING_FAX_BUCKET']).object(obj_key)
           obj.put(body: Base64.decode64(pdf[:Result]), content_type: 'application/pdf')
